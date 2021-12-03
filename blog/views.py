fc1fa7b8794ad9bb8d6bdc6fc90b17618c4a609e
ElyseRH <elyserhoran@gmail.com>
2021-12-03 18:38:22 +0000

    Add blog urls.py

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post  #use Post as its model
-    queryset = Post.objects.filter(status=1).order_by('-created_on')  #shows published posts in descending order
+    model = Post  # use Post as its model
+    queryset = Post.objects.filter(status=1).order_by('-created_on')  # shows published posts in descending order
     template_name = 'index.html'  # takes index as it's template
-    paginate_by = 6  #6 posts will appear per page
+    paginate_by = 6  # 6 posts will appear per page
