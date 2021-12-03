e647870cd5ff1aaf36818edae698d374372a1054
ElyseRH <elyserhoran@gmail.com>
2021-12-03 17:58:03 +0000

    Add class postlist to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post  #use Post as its model
+    queryset = Post.objects.filter(status=1).order_by('-created_on')  #shows published posts in descending order
+    template_name = 'index.html'  # takes index as it's template
+    paginate_by = 6  #6 posts will appear per page
