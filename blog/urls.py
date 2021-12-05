7bba27989ea8bcc4c3de6fa2f550528842435e8f
ElyseRH <elyserhoran@gmail.com>
2021-12-05 12:22:00 +0000

    Add allauth

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
 
-
+# as view is required because we are using built in class views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 # Create your views here.
@@ -8,3 +8,25 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')  # shows published posts in descending order
     template_name = 'index.html'  # takes index as it's template
     paginate_by = 6  # 6 posts will appear per page
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)  # only search published posts
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        #below checks if user has liked comments and shows
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
